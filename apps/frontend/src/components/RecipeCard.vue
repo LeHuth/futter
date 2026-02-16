fa770aa470f169dca5f9fb13c8284d6bdf6bec7f
zedong <leonard.huth@googlemail.com>
2026-02-17 00:26:12 +0100

    Add configuration files for GitToolBox and Prettier, update global styles and layout components

---
+++ apps/frontend/src/components/RecipeCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <p class="text-body-sm">Lorem Ipsum - Sub Title</p>
+    <h2 class="text-heading-md">Lorem ipsum dolor sit amet</h2>
+    <p class="text-caption flex justify-between mt-1">
+      <span> <span>12 Minutes</span><span> - </span><span>Easy</span> </span>
+      <span class="underline more">show more...</span>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid var(--gray-500);
+  background-color: var(--bg-primary);
+  border-radius: 20px;
+  padding: 14px 12px;
+}
+
+.more {
+  cursor: pointer;
+}
+</style>
---
+++ apps/frontend/src/components/RecipeListing.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import RecipeCard from '@/components/RecipeCard.vue';
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <RecipeCard v-for="i in 5" :key="i" />
+  </div>
+</template>
+
+<style scoped></style>
