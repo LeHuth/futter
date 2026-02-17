026c4fc58b1d7fe5163956142bee4fd6d26873c4
zedong <leonard.huth@googlemail.com>
2026-02-17 17:24:03 +0100

    Add motion-v and unplugin-vue-components for enhanced animations and component management

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-vue-components/vite';
+import MotionResolver from 'motion-v/resolver';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
@@ -8,7 +10,13 @@ export default defineConfig({
   devToolbar: { enabled: false },
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      /** @type {any} */ (Components({
+        dts: true,
+        resolvers: [MotionResolver()],
+      })),
+    ],
     server: {
       proxy: {
         '/api': {
--- apps/frontend/src/components/RecipeCard.vue
+++ apps/frontend/src/components/RecipeCard.vue
@@ -4,10 +4,65 @@
     <h2 class="text-heading-md">Lorem ipsum dolor sit amet</h2>
     <p class="text-caption flex justify-between mt-1">
       <span> <span>12 Minutes</span><span> - </span><span>Easy</span> </span>
-      <span class="underline more">show more...</span>
+      <span class="underline more" @click="toggleExpand()">show more...</span>
     </p>
+    <AnimatePresence>
+      <Motion
+        v-if="isExpanded"
+        :initial="{ opacity: 0, height: 0 }"
+        :animate="{ opacity: 1, height: 'auto' }"
+        :exit="{ opacity: 0, height: 0 }"
+        :transition="{ duration: 0.3 }"
+        style="overflow: hidden"
+      >
+        <div class="mt-4">
+          <p class="text-body-sm text-justify">
+            <span>Ingridents: </span> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Nullam nec sapien vel sapien rutrum congue auctor
+            pharetra est.
+          </p>
+        </div>
+        <div class="grid grid-cols-2 text-body-sm mt-4">
+          <div>
+            <p>Nutrition</p>
+            <p>Type</p>
+            <p class="mt-2">Calories</p>
+            <p>Fat</p>
+            <p>Carbohydrates</p>
+            <p>Fibers</p>
+            <p>Protein</p>
+            <p>Salt</p>
+          </div>
+          <div class="text-end">
+            <p>Per Serving</p>
+            <p>Amount</p>
+            <p class="mt-2">250 kcal</p>
+            <p>10 g</p>
+            <p>30 g</p>
+            <p>5 g</p>
+            <p>15 g</p>
+            <p>0.5 g</p>
+          </div>
+        </div>
+      </Motion>
+    </AnimatePresence>
   </div>
 </template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+import { AnimatePresence, Motion } from 'motion-v';
+
+defineComponent({
+  name: 'RecipeCard',
+});
+
+const isExpanded = ref(false);
+
+const toggleExpand = () => {
+  isExpanded.value = !isExpanded.value;
+};
+</script>
 
 <style scoped>
 .card {
